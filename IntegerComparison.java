f78ec7eb81bc5e79ec1d0892b484a84139fcbf1d
Christian Barreto <chrbarreto@gmail.com>
2018-05-04 12:36:20 -0300

    Create IntegerComparison.java

---
+++ IntegerComparison.java
@@ -0,0 +1,19 @@
+/**
+ * @author chrbarreto
+ */
+public class IntegerComparison {
+
+    public static void main(String[] args) {
+        Integer n1 = 8;
+        Integer n2 = 8;
+
+        System.out.println(n1 == n2); // true
+
+        Integer n3 = 200;
+        Integer n4 = 200;
+
+        System.out.println(n3 == n4); // false
+
+    }
+
+}
